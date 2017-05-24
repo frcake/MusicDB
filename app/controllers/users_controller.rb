8513c2f2ee05dbe8bf6206bd1e325afe2a9220ec
frcake <mpdevcake@gmail.com>
2017-05-24 22:58:43 +0300

    Change Users table column from password to password_digest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
 
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524194841) do
+ActiveRecord::Schema.define(version: 20170524195710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,7 +78,7 @@ ActiveRecord::Schema.define(version: 20170524194841) do
     t.boolean "is_admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password", default: "", null: false
+    t.string "password_digest", default: "", null: false
     t.string "password_confirmation", default: "", null: false
   end
 
