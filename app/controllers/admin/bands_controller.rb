4f83b5f597f211ba17bcac84996e43774223a031
frcake <mpdevcake@gmail.com>
2017-06-11 01:17:39 +0300

    Add update functionality to bands model

--- app/controllers/admin/bands_controller.rb
+++ app/controllers/admin/bands_controller.rb
@@ -14,10 +14,12 @@ class Admin::BandsController < AdminController
   def create
     @band = Band.new(band_params)
     if @band.save!
-      if params[:artists]
-        params[:artists].each do |artist|
-          @bandmembers = @band.bandmembers.build(artist_id: artist)
-          @bandmembers.save
+    # byebug
+      if params[:band][:artists]
+        params[:band][:artists].each do |artist|
+          @bandmembers = @band.bandmembers.build(artist_id: artist) #unless artist.empty?
+          @bandmembers.save #unless artist.empty?
+         ## byebug
         end
       end
       if params[:images]
@@ -36,17 +38,44 @@ class Admin::BandsController < AdminController
   end
 
   def edit_band
-    @band = Band.find(params[:id])
-    @artists = Artist.all.map{|x| ["#{x.firstname} #{x.lastname}" ,"#{x.id}"]}
-  end
+   @band = Band.find(params[:id])
+   @artists = Artist.all.map{|x| ["#{x.firstname} #{x.lastname}" ,"#{x.id}"]}
+
+   @count = Bandmember.where(band_id: @band.id)
+   #byebug
+   @array = []
+   if @count.size > 1
+    @count.each do |artist|
+     @artist = Artist.find(artist.artist_id).id
+     @array << @artist
+    end
+   elsif @count.size == 1
+     @count = Bandmember.find_by(band_id: @band.id)
+     @artist = Artist.find(@count.artist_id).id
+     @array << @artist
+    else
+     @array = [""]
+    end
+    #@array = [""]
+    #byebug
+   end
+   #@selected_artists = @array.map{|x| ["#{x.firstname} #{x.lastname}" ,"#{x.id}"]}
+
+
 
   def update
+   #byebug
     if @band.update_attributes(band_params)
-      flash[:success] = "Band updated!"
-      redirect_to admin_bands_path
+     if params[:band][:artists]
+       params[:band][:artists].each do |artist|
+         @bandmembers = @band.bandmembers.build(artist_id: artist) #unless artist.empty?
+         @bandmembers.save #unless artist.empty?
+        ## byebug
+       end
+     end
+     flash[:success] = "good"
+     redirect_to admin_bands_edit_path
     else
-      flash[:warning] = "Please try again"
-      redirect_to admin_bands_edit_path(@band)
     end
   end
 
@@ -57,6 +86,6 @@ class Admin::BandsController < AdminController
   end
 
   def band_params
-    params.require(:band).permit(:name,:genre,:description,:artist,:photos,:category_id,bandmember:[:band_id,:artist_id])
+    params.require(:band).permit(:name,:genre,:description,:artist,:photos,:category_id,artists:[:id])#,bandmember:[:band_id,:artist_id])
   end
 end
