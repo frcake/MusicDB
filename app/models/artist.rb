d15771860d68d4015466d7bd6b24fd59fb0d3a63
frcake <mpdevcake@gmail.com>
2017-06-12 17:26:03 +0300

    add dependent nullify Artist - Bandmembers

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,5 @@
 class Artist < ApplicationRecord
-  has_many :bandmembers
+  has_many :bandmembers , dependent: :destroy
   has_many :photos,as: :imageable, dependent: :destroy
   has_many :bands , through: :bandmembers
   belongs_to :category,optional: true
