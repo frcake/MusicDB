29b9476d57258c3ca84bc94c9ea6fcac39b1cec0
frcake <mpdevcake@gmail.com>
2017-11-09 17:26:31 +0200

    Adds new and create action to admin for user model

--- Gemfile
+++ Gemfile
@@ -59,6 +59,8 @@ gem 'select2-rails'
 gem 'discogs-wrapper'
 gem 'searchkick'
 
+gem 'ember-cli-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,11 +1,27 @@
 class Admin::UsersController < AdminController
-  before_action :set_user ,only:[:update,:destroy]
+  before_action :set_user, only: %i[update destroy]
   def index_user
     @users = User.all
   end
 
+  def new_user
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = 'Welcome to MusicDB!'
+      redirect_to admin_users_url
+
+    else
+      render 'new'
+    end
+end
+
   def user_edit
-    @user=User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def update
@@ -20,16 +36,17 @@ class Admin::UsersController < AdminController
 
   def destroy
     @user.destroy
-    flash[:success] = "User Successfuly deleted"
+    flash[:success] = 'User Successfuly deleted'
     redirect_to admin_users_path
   end
 
   private
+
   def set_user
     @user = User.find(params[:id])
   end
 
   def user_params
-    params.require(:user).permit(:username,:email,:password,:password_confirmation,:is_admin,:remember_digest)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :is_admin, :remember_digest)
   end
 end
