ee9ec7544a7914b76afd7abf38b7960d074aa5e1
frcake <mpdevcake@gmail.com>
2017-11-13 22:59:10 +0200

    Implements Record Library for each user and add delete methods for albums

--- app/controllers/record_libraries_controller.rb
+++ app/controllers/record_libraries_controller.rb
@@ -1,5 +1,5 @@
 class RecordLibrariesController < ApplicationController
-  before_action :set_record_library, only: [:show, :edit, :update, :destroy]
+  before_action :set_record_library, only: %i[show edit update destroy]
 
   # GET /record_libraries
   # GET /record_libraries.json
@@ -9,8 +9,7 @@ class RecordLibrariesController < ApplicationController
 
   # GET /record_libraries/1
   # GET /record_libraries/1.json
-  def show
-  end
+  def show; end
 
   # GET /record_libraries/new
   def new
@@ -18,22 +17,19 @@ class RecordLibrariesController < ApplicationController
   end
 
   # GET /record_libraries/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /record_libraries
   # POST /record_libraries.json
   def create
-    @record_library = RecordLibrary.new(record_library_params)
+    @record_library = current_user.record_libraries.build(album_id: params[:album_id]) # RecordLibrary.new(record_library_params)
 
-    respond_to do |format|
-      if @record_library.save
-        format.html { redirect_to @record_library, notice: 'Record library was successfully created.' }
-        format.json { render :show, status: :created, location: @record_library }
-      else
-        format.html { render :new }
-        format.json { render json: @record_library.errors, status: :unprocessable_entity }
-      end
+    if @record_library.save
+      flash[:notice] = 'Album was added to your library'
+      redirect_to root_path
+    else
+      flash[:notice] = 'Album failed to add to your library'
+      redirect_to root_path
     end
   end
 
@@ -54,21 +50,21 @@ class RecordLibrariesController < ApplicationController
   # DELETE /record_libraries/1
   # DELETE /record_libraries/1.json
   def destroy
+    # @record_library = current_user.record_library.where(user_id: current_user.id, album_id: params[:id]).destroy
     @record_library.destroy
-    respond_to do |format|
-      format.html { redirect_to record_libraries_url, notice: 'Record library was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    flash[:notice] = 'Successfully removed from your library!'
+    redirect_to root_path
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_record_library
-      @record_library = RecordLibrary.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def record_library_params
-      params.require(:record_library).permit(:user_id, :album_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_record_library
+    @record_library = RecordLibrary.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def record_library_params
+    params.require(:record_library).permit(:user_id, :album_id)
+  end
 end
