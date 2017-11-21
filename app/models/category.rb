101649a3b57b64f113f5fb717f705796d45c3cbc
frcake <mpdevcake@gmail.com>
2017-11-21 22:48:23 +0200

    Adds categories to elastic

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,21 @@
 class Category < ApplicationRecord
   acts_as_nested_set
-  has_many :albums , dependent: :nullify
+  has_many :albums, dependent: :nullify
   has_many :artists, dependent: :nullify
   has_many :songs, dependent: :nullify
   has_many :bands, dependent: :nullify
+  searchkick word_start: [:name]
+  def search_data
+    {
+      name: name
+    }.merge(search_children)
+  end
+
+  def search_children
+    {
+      band_names: bands.map(&:name),
+      album_names: albums.map(&:name)
+    }
+  end
 end
+Category.reindex
