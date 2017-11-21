a7de42a5441014bbe64805dca56eafe16c71a943
frcake <mpdevcake@gmail.com>
2017-11-22 00:39:26 +0200

    Fixes some issues with thumbnail responsiveness

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,10 @@
 class Photo < ApplicationRecord
-  #belongs_to :album, inverse_of: :photos
-  belongs_to :imageable , polymorphic: true
-  #belongs_to :artist,inverse_of: :photos
-  has_attached_file :image, styles: { large: "600x600",medium: "400x400>", thumb: "120x240>" }, default_url: 'missing/no-image.png'
+  # belongs_to :album, inverse_of: :photos
+  belongs_to :imageable, polymorphic: true
+  # belongs_to :artist,inverse_of: :photos
+  has_attached_file :image,
+                    styles: { large: '200x200', medium: '200x200>', thumb: '120x240>' },
+                    default_url: 'missing/no-image.png',
+                    convert_options: { medium: '-background white -gravity center -extent 200x200' }
   validates_attachment_content_type :image, content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']
 end
