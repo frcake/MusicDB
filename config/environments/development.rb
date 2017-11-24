d4e426f3c863cd1d4f50d668edc301e1ec03ca5e
frcake <mpdevcake@gmail.com>
2017-11-24 22:27:49 +0200

    Adds missing partial

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,27 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { from: 'brokernotify@gmail.com' }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'brokernotify@gmail.com',
+    password:             'ziouzitsou115',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
