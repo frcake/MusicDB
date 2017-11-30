59a80d5a14e2313edaa104d1b50cd7b2c205bf4f
frcake <mpdevcake@gmail.com>
2017-11-30 22:26:33 +0200

    Adds code for user_vector computation

--- db/seeds.rb
+++ db/seeds.rb
@@ -140,3 +140,10 @@
 #     record.save
 #   end
 # end
+
+# ##########USer vector##################3
+
+# category_sum_hash = {}
+# Category.all.each { |c| category_sum_hash[c.name.downcase.to_sym] = 0 unless !c.parent_id.nil? }
+
+# User.first.albums.map { |a| category_sum_hash[a.category.name.downcase.to_sym] += 1}
